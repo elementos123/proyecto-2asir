9590c5cdf3b7376a55461e2d57558e4068b43d62
elementos123 <45992848+elementos123@users.noreply.github.com>
2022-03-23 11:47:51 +0000

    Add files via upload
    Para realizar la parte de ver los logs en el apartado de estados de los servicios (estadoservicios.php)

--- index.php
+++ index.php
@@ -1,49 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="setup.css">
-    <title>Setup ControlADY</title>
-</head>
-<body>
-    <header>
-        <p>ControlADY</p>
-    </header>
-    <br><br>
-    <div id="contenedor">
-        <center>
-        <form action="" method="post">
-            <br>
-            <h2>Iniciar Sesión</h2>
-            <hr style="border-color: #CCF381;">
-            <br><br>
-            <span id="user">
-                Usuario: <input type="text" name="user" required>
-            </span>
-                
-            <br><br>
-            <span id="pass">
-                Contraseña: <input type="password" name="pass" required>
-            </span>
-
-            <br><br>
-            <input type="submit" name="enviar" value="Iniciar sesión">
-            <br>
-        </form>
-        </center>
-    </div>
-</body>
-</html>
-
-
 <?php
 
-if (isset($_POST["enviar"]))
+$text = "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure\n of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through\n weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to\n do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently\n occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to\n secure other greater pleasures, or else he endures\n pains to avoid worse pains.";
+$textotemporal = "";
+$lineaslimitesparasumarcontadorarray = 10;
+$array = array();
+$contadorstring = 0;
+$contadorarray = 0;
+$contadorparalineas = 0;
+echo strlen($text);
+while($contadorstring < strlen($text))
 {
-    require 'funciones.php';
-    ConsultarUsuarios($_POST["user"], $_POST["pass"]);
+    $textotemporal = $textotemporal . $text[$contadorstring];
+
+    if ($contadorparalineas == $lineaslimitesparasumarcontadorarray)
+    {
+        $array[$contadorarray] = $textotemporal;
+        $contadorarray++;
+        $contadorparalineas = 0;
+        $textotemporal = "";
+    }
+
+    $contadorstring++;
+    $contadorparalineas++;
+}
+
+
+for ($i=0; $i < count($array); $i++) 
+{ 
+    echo "I: " . $i . " texto: " . $array[$i] . "<br><br>";
 }
 
 ?>
